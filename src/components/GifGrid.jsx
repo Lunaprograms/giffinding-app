7539f6873c137b0110a3954471891899925895d9
Lau Luna <lunaprograms23@gmail.com>
2023-12-04 20:42:34 -0600

    agregamos la carpeta de index.js para centralizar las importaciones y mejorar la limpieza del codigo, todo funciona bien, ahora vamos adesplegar la app en netlify

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,7 +8,7 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
-      <h2>Cargando...</h2>
+      {isLoading && <h3>Cargando...</h3>}
       <div className="card-grid">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
